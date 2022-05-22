e2800eed332fde1dbc4bccab4e9aa0668a9dd683
demostanis <demostanis@protonmail.com>
2022-05-22 16:44:32 +0200

    source in /usr/src

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,7 +4,13 @@ sed -i 's#<packages>#'"$(pwd)"/packages'#' pacman.conf
 IFS=$'\n'; for m in "$(findmnt -ao TARGET | cut -d- -f2- | grep $PWD)"; do
 umount -l "$m" 2>/dev/null
 done
-rm -rf work && mkarchiso -v -w work -o out .
+rm -rf work && \
+# gotta patch mkarchiso to insert source /usr/src
+sed '5s/^/set -- -v -w work -o out .\n/;/Copying custom airootfs files.../{n;a\
+	_msg_info "Copying source to /usr/src..."\
+	mkdir -p "$pacstrap_dir/usr/src/demolinux"\
+	rsync --exclude-from=<(git -C "$profile" ls-files -oi --exclude-standard --directory) -Eav --chown=root:root -- "$profile"/* "$pacstrap_dir/usr/src/demolinux"
+}' `which mkarchiso` | bash
 # for ease of understandability
 sed -i 's#'"$(pwd)"/packages'#<packages>#' pacman.conf
 chown -R 1000:1000 out
