501c815cf72f059bc0898b682fb77345731b7125
demostanis <demostanis@protonmail.com>
2022-06-10 21:17:02 +0200

    Fix infinite rsync recursion when copying source to /usr/src because of git

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,6 +7,7 @@ done
 rm -rf work && \
 # gotta patch mkarchiso to insert source /usr/src
 sed '5s/^/set -- -v -w work -o out .\n/;/Copying custom airootfs files.../{n;a\
+	git config --global --add safe.directory "$profile"\
 	_msg_info "Copying source to /usr/src..."\
 	mkdir -p "$pacstrap_dir/usr/src/demolinux"\
 	rsync --exclude-from=<(git -C "$profile" ls-files -oi --exclude-standard --directory) -Eav --chown=root:root -- "$profile"/* "$pacstrap_dir/usr/src/demolinux"\
