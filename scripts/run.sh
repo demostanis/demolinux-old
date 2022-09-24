2645f81e62ec96d93f6f1f0cc16e26574786bcb6
demostanis <demostanis@protonmail.com>
2022-09-24 16:38:06 +0200

    s/gtk/sdl/ for QEMU
    (note that qemu still sucks with demolinux)

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 f=`mktemp /tmp/XXXXXXXrun_archiso`
 trap "rm $f" EXIT
-sed 's/-vga virtio/-device virtio-vga-gl -display gtk,gl=on/' `which run_archiso` > $f
+sed 's/-vga virtio/-device virtio-vga-gl -display sdl,gl=on/' `which run_archiso` > $f
 sed -i s/\'sdl/\'sdl,gl=on/ $f
 [ -n "$UEFI" ] && flags=u
 [ -n "$DISK" ] && sed -i '/-enable-kvm/{a -drive file='"$DISK"'\\
