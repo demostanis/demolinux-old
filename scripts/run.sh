90aa9221ccabc5099c306256dc1f500e37a7354c
demostanis <demostanis@protonmail.com>
2022-10-27 12:16:57 +0200

    Fix run script to lose privileges if run as root

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,10 @@
 #!/bin/zsh
+
+# QEMU musn't run as root
+if [ -n "$SUDO_USER" ]; then
+	exec sudo -u $SUDO_USER -- sh -c 'unset SUDO_USER && "'"$0"'"'
+fi
+
 f=`mktemp /tmp/XXXXXXXrun_archiso`
 trap "rm $f" EXIT
 sed 's/-vga virtio/-device virtio-vga-gl -display sdl,gl=on/' `which run_archiso` > $f
